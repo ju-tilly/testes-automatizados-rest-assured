29cfdcc63750bbd7db2b5a02b468cdc6c074183e
ju-tilly <juce.duarte.f@gmail.com>
2022-12-11 01:40:43 -0300

    Implementação dos testes: get por username, delete de usuário, return de request com body inválido

--- src/test/java/User/UserTests.java
+++ src/test/java/User/UserTests.java
@@ -4,10 +4,12 @@ import Entities.User;
 import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.config.LogConfig.logConfig;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
@@ -33,13 +35,14 @@ public class UserTests {
 
     @BeforeEach
     void setRequest(){
-        request = given()
+        request = given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails())) //Mostra um log de detalhes em caso de erros
                 .header("api-key","special-key")
                 .contentType(ContentType.JSON);
     }
 
     //primeiro teste - POST
     @Test
+    @Order(1)
     public void CreateNewUser_WithValidData_ReturnOk(){ //sempre colocar bem especificado o que o teste irá fazer e retorna
         request.body(user)
                 .when()
@@ -54,6 +57,7 @@ public class UserTests {
 
     //Primeiro teste - GET
     @Test
+    @Order(2)
     public void GetLogin_validUser_ReturnOk(){
         request
                 .param("username", user.getUsername())
@@ -67,5 +71,49 @@ public class UserTests {
                 .time(lessThan(2000L))
                 .and()
                 .body(matchesJsonSchemaInClasspath("loginResponseSchema.json")); //passando o caminho da schema, setado em um arquivo de configuração json
+    }
+
+    //teste - GET por username de usuário
+    @Test
+    @Order(3)
+    public void GetUserByUsername_userIsValid_ReturnOk(){
+        request
+                .when()
+                .get("/user/" + user.getUsername())
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .time(lessThan(2000L))
+                .and()
+                .body("firstName", equalTo(user.getFirstName()));
+    }
+
+    @Test
+    @Order(4)
+    public void DeleteUser_UserExists_ReturnOk(){
+        request
+                .when()
+                .delete("/user/" + user.getUsername())
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .time(lessThan(2000L))
+                .log();
+    }
+
+    @Test
+    public void CreateNewUser_WithInvalidBody_ReturnBadRequest(){
+        Response response = request
+                .body("teste")
+                .when()
+                .post("/user")
+                .then()
+                .extract().response();
+        Assertions.assertNotNull(response); //primeira validação
+        Assertions.assertEquals(400, response.statusCode()); //segunda validação
+        Assertions.assertTrue(response.getBody().asPrettyString().contains("unknown"));
+        Assertions.assertEquals(3, response.body().jsonPath().getMap("$").size());
     }
 }
